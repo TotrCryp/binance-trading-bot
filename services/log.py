a201206ab3875d53c61fcf3d3cb2f95aa374f357
Totr <totracryp@gmail.com>
2025-07-18 16:33:47 +0300

    Moved logging setup to a separate module, added handling of startup arguments

---
+++ services/log.py
@@ -0,0 +1,23 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+
+def setup_logging():
+    # Console logging configuration
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    # File logging configuration
+    file_handler = TimedRotatingFileHandler('btb_app.log', when='D', interval=1, backupCount=2)
+    file_handler.setLevel(logging.INFO)
+
+    # Log format
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    # Handlers
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)

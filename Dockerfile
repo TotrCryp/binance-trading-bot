f87022db66b0e7c5c6c92437380454b46d628f85
Totr <totracryp@gmail.com>
2025-08-01 17:14:20 +0300

    added .dockerignore Removed storing logs in a file, assumes use of stdout

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,19 @@
-FROM python:3.11-slim
+FROM python:3.9-slim
 LABEL authors="Totr"
 
-WORKDIR /app
+WORKDIR /btb_app
 
-COPY . /app
+RUN apt-get update && \
+    apt-get install -y git && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN git clone https://github.com/TotrCryp/binance-trading-bot.git /btb_app
 
 RUN mkdir -p db/databases
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-CMD ["python", "main.py"]
+ENTRYPOINT ["python", "main.py"]
+
+CMD ["-S", "BTCUSDT", "--mode", "test", "-u"]
--- services/log.py
+++ services/log.py
@@ -1,23 +1,10 @@
 import logging
-from logging.handlers import TimedRotatingFileHandler
 
 
 def setup_logging():
-    # Console logging configuration
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-
-    # File logging configuration
-    file_handler = TimedRotatingFileHandler('btb_app.log', when='D', interval=10, backupCount=2)
-    file_handler.setLevel(logging.INFO)
-
-    # Log format
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    console_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
-
-    # Handlers
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(handler)
